31fe063807bbfe154463694be14c1e1053aff808
Lluis Fita Borrell <lluis@debian-BULLSEYE-live-builder-AMD64>
2023-09-20 15:52:18 -0300

    Adding the main module to compute urban diagnostics to be called from phys/module_diagnostics_driver.F

---
+++ urbanModule/phys/module_urban_diags.f90
@@ -0,0 +1,22 @@
+MODULE module_urban_diags
+! Module to compute urban diagnostics
+!   Initiative from CORDEX Flagship Pilot Study Europe: URBan environments and Regional Climate 
+!     Change, (CORDEX FPS URB RCC, https://ms.hereon.de/cordex_fps_urban/index.php.en)
+!   L. Fita, CIMA, UBA-CONICET, IRL IFAECI CNRS/IRD, Argentina
+!  
+
+!
+! Subroutines/Functions
+!
+! urban_diags: subroutine to compute trhe urban diagnostics
+
+
+! Available diagnostics
+
+
+  SUBROUTINE urban_diags()
+  
+  END SUBROUTINE urban_diags
+
+
+END MODULE module_urban_diags
